dd49a88cef1b249187b13922050e2f4dd722ae5e
Jimin Huh <jiminhuh95@gmail.com>
2018-12-18 10:02:20 -0600

    added html and css to project

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+/* Canvas */
+
+var canvas = document.getElementById("hangmanCanvas");
+var ctx = canvas.getContext("2d");
+
+
+
+function ctx1 () {
+    ctx.moveTo(100,50);
+    ctx.lineTo(100,120);
+}
+
+ctx1();
+
